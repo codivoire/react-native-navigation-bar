8ff3a0a068dca1cf62ffea5a81e56dd1c9981b6c
Roland Edi <lambirou225@gmail.com>
2021-03-16 11:59:41 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,29 +2,27 @@ import {NativeModules, Platform} from 'react-native';
 
 const {NavigationBarColor} = NativeModules;
 
-const changeNavigationBarColor = (
-  color = String,
-  light = false,
-  animated = true,
-) => {
-  if (Platform.OS === 'android') {
-    const LightNav = light ? true : false;
-    NavigationBarColor.changeNavigationBarColor(color, LightNav, animated);
-  }
-};
-const hideNavigationBar = () => {
-  if (Platform.OS === 'android') {
-    return NavigationBarColor.hideNavigationBar();
-  } else {
-    return false;
-  }
-};
-const showNavigationBar = () => {
-  if (Platform.OS === 'android') {
-    NavigationBarColor.showNavigationBar();
-  } else {
-    return false;
+const NavigationBar = {
+  color: (color = String, light = false, animated = true) => {
+    if (Platform.OS === 'android') {
+      const LightNav = light ? true : false;
+      NavigationBarColor.changeNavigationBarColor(color, LightNav, animated);
+    }
+  },
+  show: () => {
+    if (Platform.OS === 'android') {
+      NavigationBarColor.showNavigationBar();
+    } else {
+      return false;
+    }
+  },
+  hide: () => {
+    if (Platform.OS === 'android') {
+      return NavigationBarColor.hideNavigationBar();
+    } else {
+      return false;
+    }
   }
 };
 
-export {changeNavigationBarColor, hideNavigationBar, showNavigationBar};
+export default NavigationBar;
