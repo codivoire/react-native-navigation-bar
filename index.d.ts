1ad2b7b0f2851234caa690a8a235855afa028628
Roland Edi <lambirou225@gmail.com>
2021-03-16 12:23:49 +0000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 declare module "react-native-android-navigation-bar" {
     class NavigationBar {
         color: (color: string, light: boolean, animated: boolean) => void
-        hide: () => boolean
-        hide: () => boolean
+        hide: () => any
+        hide: () => any
     }
 
     export default NavigationBar;
